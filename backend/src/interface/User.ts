98d8e7b2e97a64849f7ee42e5b34796df94bd8db
Prashant Kumar <prashantbtech.cs@gmail.com>
2025-03-19 13:36:21 +0530

    Updated user model for user profile api

--- backend/src/interface/User.ts
+++ backend/src/interface/User.ts
@@ -1,7 +1,34 @@
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 
 // Define User Interface
 export interface IUser extends Document {
     username: string;
     password: string;
+    email: string;
+    role: UserRole;
+    createdAt: Date;
+    updatedAt: Date;
+    fullName: string;
+    bio: string;
+    experience: IExperience[];
+    followers: mongoose.Types.ObjectId[];
+    following: mongoose.Types.ObjectId[];
+    postsCount: number
+    commentsCount: number;
+    likesReceived: number;
+}
+
+interface IExperience {
+    title: string;
+    company: string;
+    location: string;
+    from: Date;
+    to: Date;
+    current: boolean;
+    description: string;
+}[]
+
+export enum UserRole {
+    Admin = "admin",
+    Moderator = "moderator"
 }
--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,10 +1,32 @@
 import mongoose, { Schema, Model } from 'mongoose';
-import { IUser } from "../interface/User";
+import { IUser, UserRole } from "../interface/User";
 
 // Define Schema
 const userSchema: Schema<IUser> = new Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    role: { type: String, required: true, default: UserRole.Admin, enum: Object.values(UserRole) },
+    fullName: { type: String },
+    bio: { type: String },
+    experience: [
+        {
+            title: { type: String, required: true },
+            company: { type: String, required: true },
+            location: { type: String },
+            from: { type: Date, required: true },
+            to: { type: Date },
+            current: { type: Boolean, default: false },
+            description: { type: String },
+        },
+    ],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    postsCount: { type: Number, default: 0 },
+    commentsCount: { type: Number, default: 0 },
+    likesReceived: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
 });
 
 // Create and Export Model
